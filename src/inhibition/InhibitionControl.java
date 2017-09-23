764213ba8d1f23a3eb3a4aa1e670ea3f34a6cb7e
greaux-thomas <thomas.greaux@icloud.com>
2017-09-23 10:55:45 +0200

    Separated the 'writing results' behaviour in its own file.

--- src/inhibition/InhibitionControl.java
+++ src/inhibition/InhibitionControl.java
@@ -16,9 +16,7 @@ import java.util.Random;
 
 public class InhibitionControl {
 
-    private static final String FILENAME = "results.txt";
-
-    private PrintWriter printWriter;
+    ResWriter resWriter = new ResWriter();
 
     private static final String SQUARE = "inhibition/pictures/square.jpg";
     private static final String STAR = "inhibition/pictures/star.png";
@@ -103,7 +101,7 @@ public class InhibitionControl {
                 });
 
                 Thread.sleep(WAIT_OUT);
-                printResults();
+                resWriter.printResults(starsTime, squaresTime);
                 Platform.runLater(() -> cleanUp());
                 return null;
             }
@@ -111,22 +109,6 @@ public class InhibitionControl {
         Thread th = new Thread(task);
         th.setDaemon(true);
         th.start();
-    }
-
-    private void cleanUp() {
-        figures.clear();
-        index = 0;
-
-        anchorP.getChildren().remove(0, anchorP.getChildren().size());
-
-        testL.setText("Test over");
-        testL.setOnMouseClicked(event -> {
-        });
-        anchorP.getChildren().add(testL);
-
-        starsTime.clear();
-        squaresTime.clear();
-        imageViews.clear();
     }
 
     /**
@@ -144,7 +126,7 @@ public class InhibitionControl {
                 }
 
                 Thread.sleep(WAIT_OUT * 2);
-                printResults();
+                resWriter.printResults(starsTime, squaresTime);
                 Platform.runLater(() -> cleanUp());
 
                 return null;
@@ -172,7 +154,7 @@ public class InhibitionControl {
                 }
 
                 Thread.sleep(WAIT_OUT);
-                printResults();
+                resWriter.printResults(starsTime, squaresTime);
                 Platform.runLater(() -> cleanUp());
 
                 return null;
@@ -199,7 +181,7 @@ public class InhibitionControl {
                 }
 
                 Thread.sleep(WAIT_OUT);
-                printResults();
+                resWriter.printResults(starsTime, squaresTime);
                 Platform.runLater(() -> cleanUp());
 
                 return null;
@@ -208,6 +190,25 @@ public class InhibitionControl {
         Thread th = new Thread(task);
         th.setDaemon(true);
         th.start();
+    }
+
+    /**
+     * This method make sure there's no conflict between the differents tests
+     */
+    private void cleanUp() {
+        figures.clear();
+        index = 0;
+
+        anchorP.getChildren().remove(0, anchorP.getChildren().size());
+
+        testL.setText("Test over");
+        testL.setOnMouseClicked(event -> {
+        });
+        anchorP.getChildren().add(testL);
+
+        starsTime.clear();
+        squaresTime.clear();
+        imageViews.clear();
     }
 
     private void generateFigures(int squares, int stars) {
@@ -287,72 +288,4 @@ public class InhibitionControl {
         if (coin > 0.5) delta *= -1;
         return SIZE + delta;
     }
-
-    /**
-     * Print the results in the result file
-     */
-    private void printResults() {
-
-        try {
-            FileWriter temp = new FileWriter(FILENAME, true);
-            printWriter = new PrintWriter(temp);
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        printWriter.println("\n\n");
-
-        printWriter.println("Stars:");
-        stats(starsTime);
-
-        printWriter.println("\n\nSquares:");
-        stats(squaresTime);
-
-        printWriter.println("\n\n");
-        printWriter.println("\n#####");
-        printWriter.close();
-    }
-
-    /**
-     * Prints a list of statistics for a given list of double
-     */
-    private void stats(List<Double> list) {
-
-        if (list.isEmpty()) {
-            printWriter.println("list empty");
-            return;
-        }
-
-        Collections.sort(list);
-        double min = list.get(0);
-        double max = list.get(list.size() - 1);
-        double med = list.get(list.size() / 2);
-        double avg = 0.0;
-        for (double e : list) {
-            avg += e;
-        }
-        avg /= list.size();
-
-        double std_dev = 0.0;
-        for (double e : list) {
-            std_dev += Math.pow(e - avg, 2);
-        }
-        std_dev /= list.size();
-        std_dev = Math.sqrt(std_dev);
-
-        min = (double) Math.round(min * 100) / 100;
-        max = (double) Math.round(max * 100) / 100;
-        med = (double) Math.round(med * 100) / 100;
-        avg = (double) Math.round(avg * 100) / 100;
-        std_dev = (double) Math.round(std_dev * 100) / 100;
-
-        printWriter.println("number of elements: " + list.size());
-        printWriter.println("min = " + min);
-        printWriter.println("max = " + max);
-        printWriter.println("med = " + med);
-        printWriter.println("avg = " + avg);
-        printWriter.println("std dev = " + std_dev);
-    }
-
 }
---
+++ src/inhibition/ResWriter.java
@@ -0,0 +1,80 @@
+package inhibition;
+
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
+
+public class ResWriter {
+
+    private PrintWriter printWriter;
+    private static final String FILENAME = "results.txt";
+
+    /**
+     * Print the results in the result file
+     */
+    public void printResults(List<Double> starsTime, List<Double> squaresTime) {
+
+        try {
+            FileWriter temp = new FileWriter(FILENAME, true);
+            printWriter = new PrintWriter(temp);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        printWriter.println("\n\n");
+
+        printWriter.println("Stars:");
+        stats(starsTime);
+
+        printWriter.println("\n\nSquares:");
+        stats(squaresTime);
+
+        printWriter.println("\n\n");
+        printWriter.println("\n#####");
+        printWriter.close();
+    }
+
+    /**
+     * Prints a list of statistics for a given list of double
+     */
+    private void stats(List<Double> list) {
+
+        if (list.isEmpty()) {
+            printWriter.println("list empty");
+            return;
+        }
+
+        Collections.sort(list);
+        double min = list.get(0);
+        double max = list.get(list.size() - 1);
+        double med = list.get(list.size() / 2);
+        double avg = 0.0;
+        for (double e : list) {
+            avg += e;
+        }
+        avg /= list.size();
+
+        double std_dev = 0.0;
+        for (double e : list) {
+            std_dev += Math.pow(e - avg, 2);
+        }
+        std_dev /= list.size();
+        std_dev = Math.sqrt(std_dev);
+
+        min = (double) Math.round(min * 100) / 100;
+        max = (double) Math.round(max * 100) / 100;
+        med = (double) Math.round(med * 100) / 100;
+        avg = (double) Math.round(avg * 100) / 100;
+        std_dev = (double) Math.round(std_dev * 100) / 100;
+
+        printWriter.println("number of elements: " + list.size());
+        printWriter.println("min = " + min);
+        printWriter.println("max = " + max);
+        printWriter.println("med = " + med);
+        printWriter.println("avg = " + avg);
+        printWriter.println("std dev = " + std_dev);
+    }
+}
